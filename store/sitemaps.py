36b236aa9a590bf2d7e64dc562abbc06ccc28ee9
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2026-06-21 18:07:02 +0500

    add sitemap configuration for products, categories, and static pages

--- store/sitemaps.py
+++ store/sitemaps.py
@@ -30,7 +30,7 @@ class StaticViewSitemap(Sitemap):
     changefreq = 'daily'
 
     def items(self):
-        return ['home', 'product_list', 'deals', 'contact']
+        return ['home', 'product_list', 'deals', 'contact', 'guides_list', 'best_smart_home_security_system_2026']
 
     def location(self, item):
         return reverse(item)
